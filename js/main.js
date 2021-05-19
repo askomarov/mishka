89d166db76de4e58ad9298c4bee49d48d200888e
askomarov <askomarov13@mail.ru>
2021-05-19 23:38:59 +0300

    Updates

--- js/main.js
+++ js/main.js
@@ -40,6 +40,70 @@ const onResizeWindowCloseMenu = () => {
   )
 };
 
+
+
+let buttons = document.querySelectorAll(".slider-buttons__btn");
+const btnPrev = document.querySelector('.slider-buttons__btn--prev');
+const btnNext = document.querySelector('.slider-buttons__btn--next');
+let slides = document.querySelectorAll(".slider-list__item");
+console.log(slides);
+
+const checkBtnPrev = (slideActive) => {
+  if (slideActive.previousElementSibling !== null) {
+    btnPrev.removeAttribute("disabled");
+  } else {
+    btnPrev.setAttribute("disabled", "disabled")
+  }
+};
+const checkBtnNext = (slideActive) => {
+  if (slideActive.nextElementSibling !== null) {
+    btnNext.removeAttribute("disabled");
+  } else {
+    btnNext.setAttribute("disabled", "disabled")
+  }
+};
+
+const changeActiveSlider = () => {
+  slideActive = document.querySelector(".slider-list__item--active");
+  checkBtnPrev(slideActive);
+  checkBtnNext(slideActive);
+}
+
+let promoSlider = function () {
+  let slideActive = document.querySelector(".slider-list__item--active");
+  checkBtnPrev(slideActive);
+  checkBtnNext(slideActive);
+  let buttons = document.querySelectorAll(".slider-buttons__btn");
+  // let slides = document.querySelectorAll(".slider-list__item");
+
+
+  buttons.forEach(btn => (
+    btn.addEventListener('click', (evt) => {
+      let slideActive = document.querySelector(".slider-list__item--active");
+
+
+      if (evt.target.classList.contains('slider-buttons__btn--prev')) {
+        if (slideActive.previousElementSibling !== null) {
+          slideActive.previousElementSibling.classList.add('slider-list__item--active');
+          slideActive.classList.remove('slider-list__item--active');
+          changeActiveSlider();
+        }
+      }
+      if (evt.target.classList.contains('slider-buttons__btn--next')) {
+        // checkBtnPrev(slideActive);
+        // checkBtnNext(slideActive);
+        if (slideActive.nextElementSibling !== null) {
+          slideActive.nextElementSibling.classList.add('slider-list__item--active');
+          slideActive.classList.remove('slider-list__item--active');
+          changeActiveSlider();
+        }
+      }
+    })
+  ))
+};
+
+
+// //////////////////////////////////
 document.addEventListener("DOMContentLoaded", () => {
   body.classList.remove('no-js');
   closeMenu();
@@ -47,4 +111,5 @@ document.addEventListener("DOMContentLoaded", () => {
   window.innerWidth >= 768 ? showMenu() : closeMenu();
   // onResizeWindowCloseMenu();
   onClickMenuButtonToogleMenu();
+  promoSlider();
 })
